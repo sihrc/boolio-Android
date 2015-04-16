189bd5a28afc0c80dfa39ea2dacd95320b617792
acejang1994 <byumho.jang@students.olin.edu>
2015-04-16 00:50:11 -0400

    setting up logging fragment

---
+++ app/src/main/java/io/boolio/android/fragments/tutorials/LoginFragment.java
@@ -0,0 +1,35 @@
+package io.boolio.android.fragments.tutorials;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import io.boolio.android.R;
+import io.boolio.android.fragments.BoolioFragment;
+
+/**
+ * Created by james on 4/15/15.
+ */
+public class LoginFragment extends BoolioFragment {
+
+    public static LoginFragment newInstance(){
+        LoginFragment fragment= new LoginFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        // Find Views and Buttons
+        EditText loginEmail = (EditText) rootView.findViewById(R.id.login_email);
+        EditText loginPassword = (EditText) rootView.findViewById(R.id.login_password);
+        Button loginButton = (Button) rootView.findViewById(R.id.login_button);
+
+        return rootView;
+    }
+}
