d5a4dcaa05fdbb58053292139ae6e687a2589a35
Chris Lee <sihrc.c.lee@gmail.com>
2015-04-30 13:43:29 -0400

    FIX: Production key

--- app/src/main/java/io/boolio/android/helpers/FacebookAuth.java
+++ app/src/main/java/io/boolio/android/helpers/FacebookAuth.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
