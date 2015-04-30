a2cc471b405c3a274c2a102f499056c52ca39abb
Chris Lee <sihrc.c.lee@gmail.com>
2015-04-30 13:54:47 -0400

    ADD: Deploy scripts

---
+++ genApk.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+##############################################################################
+##
+##  Generate Release APK Script
+##
+##############################################################################
+
+rm ./*.apk
+./gradlew --stacktrace assembledebug
+
+user_name=$(whoami)
+
+if (($#==0)); then
+    branch_name=$(git symbolic-ref -q HEAD)
+    branch_name=${branch_name##refs/heads/}
+    branch_name=${branch_name:-HEAD}
+else
+    branch_name=${1}
+fi
+
+DATE=`date +%m-%d@%H:%M`
+name="HangTime-"${branch_name}"-staging-${user_name}"$DATE".apk"
+
+mv "HangTime/app/build/outputs/apk/app-debug.apk" $name
+
+curl -F "file=@"$name -F channels=G03TSBRV8 -F token=xoxp-2417793652-2417793654-2760868112-82d164 https://slack.com/api/files.upload
--- genRelease.sh
+++ genRelease.sh
@@ -1,1 +1,19 @@
-genRelease.sh
+rm ./*.apk
+./gradlew assemblerelease
+
+user_name=$(whoami)
+
+if (($#==0)); then
+    branch_name=$(git symbolic-ref -q HEAD)
+    branch_name=${branch_name##refs/heads/}
+    branch_name=${branch_name:-HEAD}
+else
+    branch_name=${1}
+fi
+
+DATE=`date +%m-%d@%H:%M`
+name="Boolio-"${branch_name}"-production-${user_name}-"$DATE".apk"
+
+mv "app/build/outputs/apk/app-release.apk" $name
+
+curl -F "file=@"$name -F channels=G03TSBRV8 -F token=xoxp-2417793652-2417793654-2760868112-82d164 https://slack.com/api/files.upload
