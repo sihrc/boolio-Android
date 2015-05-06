f61c2241f3aeb763aca3a03fee7c9221a9ff7802
Chris Lee <sihrc.c.lee@gmail.com>
2015-05-06 06:31:26 -0400

    FIX: highlight answer

--- app/src/main/java/io/boolio/android/adapters/BoolioAnswerAdapter.java
+++ app/src/main/java/io/boolio/android/adapters/BoolioAnswerAdapter.java
@@ -1,22 +1,43 @@
 package io.boolio.android.adapters;
 
 import android.content.Context;
+import android.view.View;
 
+import io.boolio.android.R;
+import io.boolio.android.helpers.BoolioUserHandler;
 import io.boolio.android.models.Question;
 
 /**
  * Created by james on 4/24/15.
  */
 public class BoolioAnswerAdapter extends BoolioAdapter {
+    int rightColor, leftColor, white;
 
     public BoolioAnswerAdapter(Context context) {
         super(context);
+        this.rightColor = context.getResources().getColor(R.color.feed_question_left);
+        this.leftColor = context.getResources().getColor(R.color.feed_question_right);
+        this.white = context.getResources().getColor(android.R.color.transparent);
     }
-
 
     @Override
     public void fillContent(QuestionHolder holder, Question question) {
-        holder.leftAnswer.setText(String.valueOf(question.left + " (" + question.leftCount + ")"));
-        holder.rightAnswer.setText(String.valueOf(question.right + " (" + question.rightCount + ")"));
+        String left = String.valueOf(question.left + " (" + question.leftCount + ")");
+        String right = String.valueOf(question.right + " (" + question.rightCount + ")");
+        holder.leftAnswer.setText(left);
+        holder.rightAnswer.setText(right);
+        holder.highLeft.setText(left);
+        holder.highRight.setText(right);
+
+        if (question.usersWhoLeft.contains(BoolioUserHandler.getInstance(context).getUser().userId)) {
+            holder.highLeft.setVisibility(View.VISIBLE);
+            holder.highRight.setVisibility(View.GONE);
+        } else if (question.usersWhoRight.contains(BoolioUserHandler.getInstance(context).getUser().userId)) {
+            holder.highRight.setVisibility(View.VISIBLE);
+            holder.highLeft.setVisibility(View.GONE);
+        } else {
+            holder.highLeft.setVisibility(View.GONE);
+            holder.highRight.setVisibility(View.GONE);
+        }
     }
 }
